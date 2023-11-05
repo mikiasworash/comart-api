f5d2dbb7bc648b6f60c6bfe129f31f9dfb18dd1e
mikias-worash <mikias.worash.a@gmail.com>
2023-11-05 17:07:14 +0300

    cart update

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -3,16 +3,35 @@ import Cart from "../models/cartModel.js";
 import User from "../models/userModel.js";
 
 // @desc Add cart
-// router POST /api/cart
+// router POST /api/cart/:id
 // @access Private
 const addCart = asyncHanlder(async (req, res) => {
+  req.body.amount = 1;
   const cart = await Cart.create(req.body);
 
   if (cart) {
-    res.status(201).json({ cart });
+    res.status(201).json(cart);
   } else {
     res.status(400);
     throw new Error("adding cart failed");
+  }
+});
+
+// @desc Update cart
+// router PUT /api/cart/:id
+// @access Private
+const updateCart = asyncHanlder(async (req, res) => {
+  let cart = await Cart.findById({ user: req.params.id });
+  // Make sure cart exists
+  if (!cart) {
+    res.status(400);
+    throw new Error("Cart not found");
+  } else {
+    cart = await Cart.findByIdAndUpdate(cart._id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({ cart });
   }
 });
 
@@ -36,7 +55,7 @@ const getCart = asyncHanlder(async (req, res) => {
     throw new Error("Cart not found for this user");
   }
 
-  return res.status(200).json({ cart });
+  return res.status(200).json(cart);
 });
 
-export { getCart, addCart };
+export { getCart, addCart, updateCart };
